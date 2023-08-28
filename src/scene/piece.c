2628d8a8bb630cec6aac481d089c5cfc0c1422ed
SageScroll18144 <ffss@cin.ufpe.br>
2023-08-28 14:38:29 -0300

    peões pretos

--- src/scene/piece.c
+++ src/scene/piece.c
@@ -4,18 +4,31 @@
 Texture2D piece_pawn_white[QTD_PAWN];
 Vector2 pos_pawn_white[QTD_PAWN];
 
+Texture2D piece_pawn_black[QTD_PAWN];
+Vector2 pos_pawn_black[QTD_PAWN];
+
 void setup_pieces(void){
     //white
-    for(int i = 0 ; i < sizeof(piece_pawn_white) / sizeof(Texture2D); i++) {
+    for(int i = 0 ; i < QTD_PAWN; i++) {
         piece_pawn_white[i] = LoadTexture("../assets/SVG No shadow/pawn.png");
         pos_pawn_white[i] = (Vector2){(float)(POS_X_INITAL + i * DISTANCE_PIECE), POS_Y_INITAL + (6 * DISTANCE_PIECE)};
+    
+        piece_pawn_black[i] = LoadTexture("../assets/SVG No shadow/pawn_b.png");
+        pos_pawn_black[i] = (Vector2){(float)(POS_X_INITAL + i * DISTANCE_PIECE), POS_Y_INITAL + (1 * DISTANCE_PIECE)};
+
     }
 }
 
 void draw_pieces(void){
-    for(int i = 0 ; i < sizeof(piece_pawn_white) / sizeof(Texture2D); i++) DrawTextureRec(piece_pawn_white[i], (Rectangle){0, 0, 40, 50}, pos_pawn_white[i], WHITE);
+    for(int i = 0 ; i < QTD_PAWN; i++) {
+        DrawTextureRec(piece_pawn_white[i], (Rectangle){0, 0, 40, 50}, pos_pawn_white[i], WHITE);
+        DrawTextureRec(piece_pawn_black[i], (Rectangle){0, 0, 40, 50}, pos_pawn_black[i], WHITE);
+    }
 }
 
 void unload_pieces(void){
-    for(int i = 0 ; i < sizeof(piece_pawn_white) / sizeof(Texture2D); i++) UnloadTexture(piece_pawn_white[i]);
+    for(int i = 0 ; i < QTD_PAWN; i++) {
+        UnloadTexture(piece_pawn_white[i]);
+        UnloadTexture(piece_pawn_black[i]);
+    }
 }
