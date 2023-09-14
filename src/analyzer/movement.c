bdcd55af25f7ffdfe1923eceb87553e22612d0d6
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-13 23:50:15 -0300

    implementando uma máquina de estados para saber qual foi o movimento do usuario no tabuleiro

---
+++ src/analyzer/movement.c
@@ -0,0 +1,71 @@
+#include "movement.h"
+#include "../data_board/board.h"
+#include <string.h>
+#include <stdio.h>
+
+movement ans;
+image_board initial_image;
+image_board final_image;
+
+int sequential_flag;
+
+void build_analyzer(void){
+    sequential_flag = 0;
+
+    ans.x0 = -1;
+    ans.xF = -1;
+    ans.y0 = -1;
+    ans.yF = -1;
+
+    strcpy(ans.movement_, "XXXX");
+}
+
+int counter_piece(image_board board){
+    int acc = 0;
+    for(int i = 0; i < 8; i++) for(int j = 0; j < 8; j++) if(board.board[i][j] != 'x') acc++;
+    return acc;
+}
+
+void analyzer(void){
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
+            if(initial_image.board[i][j] != final_image.board[i][j]){
+                if(initial_image.board[i][j] != 'x') {
+                    ans.x0 = i;
+                    ans.y0 = j;
+                } else {
+                    ans.xF = i;
+                    ans.yF = j;
+                }
+
+            }
+            
+        }
+    }
+    sprintf(ans.movement_, "%c%d%c%d", letters[ans.x0], numbers[ans.y0], letters[ans.xF], numbers[ans.yF]);
+}
+
+void find_movement_FSM(image_board curr_board, int how_many_piece){
+    switch (sequential_flag)
+    {
+        case 0:
+            if(how_many_piece > counter_piece(curr_board)) sequential_flag = 1;
+            else initial_image = curr_board;
+            break;
+        
+        case 1:
+            if(how_many_piece == counter_piece(curr_board)) sequential_flag = 2;
+            break;
+
+        case 2:
+            final_image = curr_board;
+            analyzer();
+            sequential_flag = 0;
+            break;
+        
+    }
+}
+
+movement get_answer(void){
+    return ans;
+}
---
+++ src/analyzer/movement.h
@@ -0,0 +1,20 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+#include "../data_board/board.h"
+
+typedef struct{
+    int x0, xF, y0, yF;
+    char movement_[5];
+}movement;
+
+const char letters[] = {'A','B','C','D','E', 'F', 'G', 'H'};
+const int numbers[] = {8,7,6,5,4,3,2,1}; 
+
+void build_analyzer(void);
+int counter_piece(image_board board);
+void analyzer(void);
+void find_movement_FSM(image_board curr_board, int how_many_piece);
+movement get_answer(void);
+
+#endif
