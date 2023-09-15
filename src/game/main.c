230ca358da2c064301de4949f2ff5981575c01ae
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-15 00:20:56 -0300

    somente a gui

--- src/game/main.c
+++ src/game/main.c
@@ -1,28 +1,12 @@
 #include "raylib.h"
 #include "../scene/background.h"
 #include "../scene/piece.h"
-#include "../serial/serial_com.h"
-#include "../analyzer/movement.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-
-#define HOW_MANY_PIECE 1
 
 int main(void) {
-
-    open_serial();
-    setup_com();
-
-    build_analyzer();
-
-    movement curr_movement;
-    char last_movement[110] = "";
-
     const int screenWidth = 800;
     const int screenHeight = 800;
 
-    InitWindow(screenWidth, screenHeight + 100, "bit-chess | Board Status");
+    InitWindow(screenWidth, screenHeight, "bit-chess | Board Status");
 
     setup_background(screenWidth, screenHeight);
     setup_pieces();
@@ -30,35 +14,20 @@ int main(void) {
     SetTargetFPS(60);
 
     char board[8][8];
-    int turn = 1;
+    char initPosBlack[] = { 't', 'c', 'b', 'q', 'k', 'b', 'c', 't' };
+    char initPosWhite[] = { 'T', 'C', 'B', 'Q', 'K', 'B', 'C', 'T' };
+    for(int i=0;i<8;i++) for(int j=0;j<8;j++) board[i][j] = 'o';
 
+    for(int i=0;i<8;i++) {
+        board[1][i] = 'p';
+        board[6][i] = 'P';
+        board[7][i] = initPosWhite[i];
+        board[0][i] = initPosBlack[i];
+    }
     while (!WindowShouldClose()){
         // Update
         //----------------------------------------------------------------------------------
-        
-        
-        printf("\n-----------------------\n");
-        image_board buf;
-        int size_read = read_data(&buf); 
-        if(size_read == sizeof(image_board)){
-            for(int i=0;i<8;i++) {
-                for(int j=0;j<8;j++){
-                    board[i][j] = buf.board[i][j];
-                    printf("%c", board[i][j]);
-                }
-                printf("\n");
-            }
-        } 
-        printf("\n-----------------------\n");
 
-        //analisa o último movimento
-        find_movement_FSM(buf, HOW_MANY_PIECE);
-        curr_movement = get_answer();
-        if((curr_movement.x0 != curr_movement.xF || curr_movement.y0 != curr_movement.yF) && (curr_movement.x0 != -1 && curr_movement.xF != -1 && curr_movement.y0 != -1 && curr_movement.yF != -1 )) {
-            sprintf(last_movement, "Last movement: %s (%d, %d) -> (%d, %d)\n", curr_movement.movement_, curr_movement.x0, curr_movement.y0, curr_movement.xF, curr_movement.yF);
-            printf("last movement: %s (%d, %d) -> (%d, %d)\n", curr_movement.movement_, curr_movement.x0, curr_movement.y0, curr_movement.xF, curr_movement.yF);
-        
-        }
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -69,20 +38,13 @@ int main(void) {
 
             draw_background();
             draw_pieces(board);
-            
-            DrawRectangle(0, screenHeight, screenWidth, screenHeight + 100, Fade(ORANGE, 0.5f));
-            DrawRectangleLines(0, screenHeight, screenWidth, screenHeight + 100, ORANGE);
-            DrawText(last_movement, 10, screenHeight + 10, 20, DARKGRAY);
-            DrawText((turn) ? "White turn" : "Black turn", 10, screenHeight + 50, 20, DARKGRAY);
-            
+
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     unload_background();
     unload_pieces();
-
-    close_serial_connection();
 
     CloseWindow();                // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
